c86cbcda8904796dd5c8b042e2462516caad8e22
root <root@jenkins.us-central1-a.c.eternal-ruler-257808.internal>
2019-11-23 10:28:10 +0000

    terraform

---
+++ atmosphere/spring-boot-samples/spring-boot-sample-atmosphere/Dockerfile
@@ -0,0 +1,14 @@
+# Base Image
+FROM openjdk:8
+
+# Create DIR
+RUN mkdir -p /usr/src/myapp
+
+# COPY APPLICTAION COde
+COPY target/spring-boot-sample-atmosphere-1.4.0.BUILD-SNAPSHOT.jar    /usr/src/myapp
+
+# Setup Working DIR
+WORKDIR /usr/src/myapp
+
+# Start the Bot Service
+CMD ["java", "-jar", "spring-boot-sample-atmosphere-1.4.0.BUILD-SNAPSHOT.jar"]
---
+++ terraform/local_exec.tf
@@ -0,0 +1,40 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "backend" {
+  ami                    = "ami-04763b3055de4860b"
+  instance_type          = "t2.micro"
+  key_name               = "${var.key_name}"
+  vpc_security_group_ids = ["${var.sg-id}"]
+
+}
+
+resource "null_resource" "remote-exec-1" {
+    connection {
+    user        = "ubuntu"
+    type        = "ssh"
+    private_key = "${file(var.pvt_key)}"
+    host        = "${aws_instance.backend.public_ip}"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get install python sshpass -y",
+    ]
+  }
+}
+
+resource "null_resource" "ansible-main" {
+provisioner "local-exec" {
+  command = <<EOT
+        sleep 100;
+        > jenkins-ci.ini;
+        echo "[jenkins-ci]"| tee -a jenkins-ci.ini;
+        export ANSIBLE_HOST_KEY_CHECKING=False;
+        echo "${aws_instance.backend.public_ip}" | tee -a jenkins-ci.ini;
+        ansible-playbook -e  sshKey=${var.pvt_key} -i jenkins-ci.ini ./ansible/setup-backend.yaml -u ubuntu -v
+    EOT
+}
+}
